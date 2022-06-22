d441b81c53e021c285a6a9ccd6760d5bf28a21d0
elnur <elwebdev@mail.ru>
2022-06-22 17:03:18 +0500

    add components Dialogs

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
 import MyProfile from './components/Profile/MyProfile/MyProfile'
 import AppStyle from './styleApp/App.module.css'
+import Dialogs from "./components/Dialogs/Dialogs";
 
 const App = () => {
     return (
         <div className={AppStyle.appWrapper}>
             <Header/>
             <Navbar/>
-            <MyProfile/>
+            <div className={AppStyle.appWrapperContet}>
+                <MyProfile/>
+                {/*<Dialogs/>*/}
+            </div>
         </div>
     )
 }
---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import DialogsStyle from './Dialogs.module.css'
+const Dialogs = () => {
+    return (
+        <div>
+            Dialogs
+        </div>
+    );
+};
+
+export default Dialogs;
