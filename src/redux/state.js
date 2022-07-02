78421962889f9724168e87dc1ad9d87ceb778340
elnur <elwebdev@mail.ru>
2022-07-02 21:32:23 +0500

    delete render.js

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,8 @@
-import {rerenderEntireTree} from "../render";
+let rerenderEntireTree = () => {
+    console.log('state changed')
+}
+
+
 
 let state =  {
     profilePage: {
@@ -25,7 +29,7 @@ let state =  {
 }
 
 
-export let addPost = () => {
+export const addPost = () => {
     let newPost = {
         id: 3,
         message: state.profilePage.newPostText,
@@ -37,9 +41,13 @@ export let addPost = () => {
     rerenderEntireTree(state)
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
     state.profilePage.newPostText = newText
     rerenderEntireTree(state)
+}
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer
 }
 
 
